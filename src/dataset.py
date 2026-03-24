b55fcb67ae4c5a08897fa5966470bd39c710f9a2
Borek <bartek.operacz@o2.pl>
2026-03-24 12:22:38 +0100

    feat: dataset class

---
+++ src/dataset.py
@@ -0,0 +1,39 @@
+from torch.utils.data import Dataset
+import os
+from PIL import Image
+from torch import Tensor, float32
+import torchvision.transforms.v2 as v2
+
+
+class CustomImageDataset(Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir
+        self.transform = transform
+        self.image_paths = []
+        self.labels = []
+        
+        for label, class_name in enumerate(os.listdir(data_dir)):
+            class_dir = os.path.join(data_dir, class_name)
+            if os.path.isdir(class_dir):
+                for img_name in os.listdir(class_dir):
+                    self.image_paths.append(os.path.join(class_dir, img_name))
+                    self.labels.append(label)
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert("RGB")
+        label = self.labels[idx]
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        if not isinstance(image, Tensor):
+            image = v2.functional.to_image(image)
+
+        if image.dtype != float32:
+            image = v2.functional.to_dtype(image, dtype=float32, scale=True)
+            
+        return image, label
